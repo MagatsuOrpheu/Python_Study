c1d1a34f61cea995db029583333924f7ead2d5ee
JonathanMelo <jonathan.gabrielfm@gmail.com>
2023-12-02 08:11:02 -0300

    Delete Alg_examples directory

--- Alg_examples/Module.py
+++
@@ -1,7 +0,0 @@
-# Modulos em Random #
-
-import random
-
-print(random.randint(10, 100))
-print(random.choice(range(10)))
-print(random.sample(range(1000), k=12))
--- Alg_examples/true.py
+++
@@ -1,2 +0,0 @@
-qtde_dias = int(input('digite x:'))
-print(type(qtde_dias))
